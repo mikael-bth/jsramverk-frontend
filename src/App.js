d7a44506d432218602e5966d00cdb97478079692
mikael-bth <mikmen1109@gmail.com>
2022-09-06 18:47:30 +0200

    Kopplade en funktion till spara knappen som skriver ut innehållet i editorn i konsolen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import "trix";
+import "trix/dist/trix.css";
+
+import { TrixEditor } from "react-trix";
+
+function save() {
+  const trixEditor = document.querySelector("trix-editor")
+  console.log(trixEditor.innerHTML)
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>JSRamverk Editor</h1>
       </header>
+      <div className="App-editor">
+        <div className="App-toolbar">
+          <input id="save" name="save" value={"Spara"} onClick={save}></input>
+        </div>
+        <TrixEditor id="trixEditorContent" className="trix-editor" toolbar="trix-toolbar"></TrixEditor>
+      </div>
+      <footer className="App-footer">
+        <h3>JSRamverk Editor : Mikael Menonen</h3>
+      </footer>
     </div>
   );
 }
