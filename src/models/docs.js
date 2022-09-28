b31d0d69ef1a1bf6ccc33d55b5b75e94cb6cbbc6
mikael-bth <mikmen1109@gmail.com>
2022-09-28 18:58:50 +0200

    Uppdatera docs modellen så man kan nu hämta individuella docs, skapa nya och uppdatera docs från backend

--- src/models/docs.js
+++ src/models/docs.js
@@ -5,6 +5,33 @@ const docs = {
 
         return result.data;
     },
+
+    getDoc: async function getDoc(name) {
+        const response = await fetch(`localhost:1337/doc/${name}`);
+        const result = await response.json();
+
+        return result.data;
+    },
+
+    createDoc: async function createDoc(newDoc) {
+        const response = await fetch('localhost:1337/create', {
+            method: "POST",
+            body: JSON.stringify(newDoc),
+            headers: {
+                'content-type': 'application/json'
+            },
+        });
+    },
+
+    updateDoc: async function updateDoc(updatedDoc) {
+        const response = await fetch('localhost:1337/update', {
+            method: "PUT",
+            body: JSON.stringify(updatedDoc),
+            headers: {
+                'content-type': 'application/json'
+            },
+        });
+    },
 };
 
 export default docs;
