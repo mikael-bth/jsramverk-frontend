1c4231d6aeecfef63f1b666b9b4c2911f4d4bf65
mikael-bth <mikmen1109@gmail.com>
2022-10-31 00:03:43 +0100

    Fixade en tidigare bugg med testerna och la till en test för se att createpdf knappen fanns.

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,8 @@
 import { render, screen } from '@testing-library/react';
+import { TrixEditor } from "react-trix";
+import "trix";
 import App from './App';
+import pdf from './models/pdf';
 
 test('Load document button exists ', () => {
   render(<App />);
@@ -17,4 +20,10 @@ test('Document select exists ', () => {
   const { container } = render(<App />);
 
   expect(container.querySelector('#docSelect')).toBeInTheDocument();
+});
+
+test('Create PDF button exists ', () => {
+  const { container } = render(<App />);
+
+  expect(container.querySelector('#createPDF')).toBeInTheDocument();
 });
--- src/models/pdf.js
+++ src/models/pdf.js
@@ -1,10 +1,9 @@
 import jsPdf from 'jspdf';
 
 const pdf = {
-    savePDF: async function savePDF(editor, docName) {
+    savePDF: function savePDF(editor, docName) {
         const document = editor.editor.getDocument();
         const documentText = document.toString();
-        console.log(documentText);
         const pdf = new jsPdf();
         pdf.text(documentText, 10, 10);
         pdf.setFontSize(16);
